ce69af456f7ec4facaaeaff89623f1acfe8825dd
njdeane <njdeane7@gmail.com>
2021-08-07 17:38:48 +1000

    Added a "Toast" which is similar to UIAlert to MainActivity.java

--- app/src/main/java/com/nicdeane/bmicalculator/MainActivity.java
+++ app/src/main/java/com/nicdeane/bmicalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.nicdeane.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this,"Pirates was here!",Toast.LENGTH_LONG).show();
+
+
     }
 }
