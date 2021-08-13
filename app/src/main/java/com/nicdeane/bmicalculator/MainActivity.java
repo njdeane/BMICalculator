08f6f6a670063eed4a48431f12154ed542d5357d
njdeane <njdeane7@gmail.com>
2021-08-13 11:55:15 +1000

    Connect xml to main via setting id's to variables.

--- app/src/main/java/com/nicdeane/bmicalculator/MainActivity.java
+++ app/src/main/java/com/nicdeane/bmicalculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.nicdeane.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById();
+        RadioButton male = findViewById(R.id.radio_button_male);
+        RadioButton female = findViewById(R.id.radio_button_female);
+        EditText age = findViewById(R.id.edit_text_age);
+        EditText feet = findViewById(R.id.edit_text_feet);
+        EditText inches = findViewById(R.id.edit_text_inches);
+        EditText weight = findViewById(R.id.edit_text_weight);
+        Button calculate = findViewById(R.id.button_calculate);
+        TextView resultText = findViewById(R.id.text_view_result);
+
 
 
     }
