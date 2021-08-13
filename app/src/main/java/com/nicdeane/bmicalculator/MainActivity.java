157617bf7b76daf1307421561054be2747afba2d
njdeane <njdeane7@gmail.com>
2021-08-13 13:48:06 +1000

    Refactor MainActivity.java

--- app/src/main/java/com/nicdeane/bmicalculator/MainActivity.java
+++ app/src/main/java/com/nicdeane/bmicalculator/MainActivity.java
@@ -12,28 +12,47 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioButton male;
+    private RadioButton female;
+    private EditText age;
+    private EditText feet;
+    private EditText inches;
+    private EditText weight;
+    private Button calculate;
+    private TextView resultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setupButtonClickListener();
 
-        RadioButton male = findViewById(R.id.radio_button_male);
-        RadioButton female = findViewById(R.id.radio_button_female);
-        EditText age = findViewById(R.id.edit_text_age);
-        EditText feet = findViewById(R.id.edit_text_feet);
-        EditText inches = findViewById(R.id.edit_text_inches);
-        EditText weight = findViewById(R.id.edit_text_weight);
-        Button calculate = findViewById(R.id.button_calculate);
-        TextView resultText = findViewById(R.id.text_view_result);
+    }
 
+    private void findViews() {
+        male = findViewById(R.id.radio_button_male);
+        female = findViewById(R.id.radio_button_female);
+        age = findViewById(R.id.edit_text_age);
+        feet = findViewById(R.id.edit_text_feet);
+        inches = findViewById(R.id.edit_text_inches);
+        weight = findViewById(R.id.edit_text_weight);
+        calculate = findViewById(R.id.button_calculate);
+        resultText = findViewById(R.id.text_view_result);
+    }
+
+    private void setupButtonClickListener() {
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Pirates are here again", Toast.LENGTH_LONG).show();
+                calculateBMI();
             }
         });
+    }
 
-
+    private void calculateBMI() {
 
     }
+
+
 }
